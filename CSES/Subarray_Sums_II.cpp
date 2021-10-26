cfb279c2e922e2c916f2bcd8e5bcd90a7ee30cab
dwu1011 <daviddwu22@gmail.com>
2021-10-25 22:51:40 -0400

    added template

---
+++ CSES/Subarray_Sums_II.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using str = string;
+using vi = vector<int>;
+#define pb push_back
+#define f first
+#define s second
+#define all(x) begin(x), end(x)
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	ll n,x;
+	ll arr[100000];
+	scanf("%d %d", &n, &x);
+	arr.resize(n);
+
+	
+}
